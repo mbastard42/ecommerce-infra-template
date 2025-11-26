79d000b2879d92dc35df32212ed5c8ef1cae62d0
mbastard42 <matteobastard@gmail.com>
2025-11-26 15:53:44 +0100

    odoo_arch

---
+++ apps/admin/odoo/addons/setup/__manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'Custom Odoo Setup',
+    'version': '1.0',
+    'author': 'M4BA',
+    'depends': [
+        'base',
+        'stock',
+        'product',
+        'sale_management',
+        'contacts',
+        'server_environment',
+        'fs_storage',
+        'fs_attachment',
+    ],
+    'auto_install': True,
+    'installable': True,
+}
